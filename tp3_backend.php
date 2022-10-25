dfe57e0dd9fe850cc7d599dce61da3b691d8a354
Agustin Neira <104187012+AgustinNeira@users.noreply.github.com>
2022-10-25 03:38:05 -0300

    Trabajo practico n3

---
+++ tp3_backend.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+</head>
+<body>
+
+ <h1>Ejercicio 1</h1>
+<?php
+print "<p>Comienzo</p>\n";
+for ($i = 1; $i < 100; $i++) {
+    print "<p>$i</p>";
+}
+print "<p> Ejercicio Finalizado</p>";
+?>
+<h1>Ejercicio 2</h1>
+<?php
+print "<p>Comienzo</p>\n";
+for ($i = 100; $i > 1; $i--) {
+    print "<p>$i</p>";
+}
+print "<p> Ejercicio Finalizado</p>";
+?>
+
+<h1>Ejercicio 3</h1>
+<?php
+print "<p>Comienzo</p>\n";
+for ($n = 0; $n <= 100; $n = $n + 2) {
+    print "<p>$n</p>";
+}
+print "<p> Ejercicio Finalizado</p>";
+?>
+<h1>Ejercicio 4</h1>
+<?php
+print "<p>Comienzo</p>\n";
+for ($n = 1; $n <= 100; $n = $n + 2) {
+    print "<p>$n</p>";
+}
+print "<p> Ejercicio Finalizado</p>";
+?>
+<h1>Ejercicio 5</h1>
+<?php
+print "<p>Comienzo</p>\n";
+for ($i = 0; $i <= 20; $i++) {
+    print "<p>$i</p>";
+}
+print "<p> Ejercicio Finalizado</p>";
+?>
+<h1>Ejercicio 6</h1>
+<?php
+print "<p>Comienzo</p>\n";
+for ($n = 0; $n <= 20; $n = $n + 2) {
+    print "<p>$n</p>";
+}
+print "<p> Ejercicio Finalizado</p>";
+?>
+
+</body>
+</html>
